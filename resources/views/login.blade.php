5455195fb6aaa547420c041cb7d67d07fc1c2750
Mourad Yaou <91010360+MouradFrz@users.noreply.github.com>
2022-10-07 21:13:04 +0100

    vite

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,7 +7,7 @@
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    @vite('resources/css/extra.css')
+    {{-- @vite('resources/css/extra.css') --}}
 </head>
 <body>
     <div>
